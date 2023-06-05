61481154d7842407c80a5c7cc7e68d1fc615f22b
sea <274623241@qq.com>
2023-06-05 16:22:07 +0800

    one

---
+++ src/test/java/com/example/springboot/Test.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+/**
+ * ClassName: Test
+ * Package: com.example.springboot
+ * Description:
+ *
+ * @Author: small_liu
+ * @Create: 2023/6/5  16:20
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
